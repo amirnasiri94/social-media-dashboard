bb0dfb1ad4789c644240269d961abb215cca9010
Amir <amirnasiri9473@gmail.com>
2022-05-28 13:54:49 +0430

    add counter up

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// dark mode
 if(localStorage.getItem("flag") === "dark") {
     document.body.classList.toggle("dark");
     document.querySelector(".toggle-box > input").checked = true;
@@ -11,3 +12,35 @@ document.querySelector(".toggle-box > input").addEventListener("click", () => {
         localStorage.setItem("flag", "light")
     }
 });
+
+// counter up
+const numbers = document.querySelectorAll(".social-media__box__followers__number");
+
+numbers.forEach(number => {
+    number.innerText = '0';
+    const targetNumber = +number.getAttribute('data-target');
+    let count = 0;
+    const increment = targetNumber / 300;
+
+
+    const updateCounter = () => {
+        if (count < targetNumber ) {
+            count += Math.ceil(increment);
+            number.innerText = count;
+            setTimeout(updateCounter, 1);
+        } else {
+            number.innerText = showTargetNumber(targetNumber);
+        }
+    }
+    updateCounter();
+
+
+    const showTargetNumber = targetNumber => {
+        if (targetNumber < 10000) {
+            return targetNumber
+        } else {
+            const smallerTargerNumber = targetNumber / 1000;
+            return (smallerTargerNumber.toFixed(1)) + "k"
+        }
+    }
+})
